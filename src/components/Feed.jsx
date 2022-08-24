514ce869cb7be629cd977bfee32785292656bfa2
David Poslušný <david.poslusny@email.cz>
2022-08-24 12:42:40 +0200

    implemented basic axios GET request to fetch data from API to dynamically render content on homepage

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,14 +1,19 @@
 import { Box } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react'
 import { Post } from './Post'
+import axios from 'axios'
 
 export const Feed = () => {
+
+  const [post, setPost] = useState([])
+
+  axios.get('https://daiton-food-square-api.herokuapp.com/api/v1/posts/getAll').then((response) => {
+    setPost(response.data)
+  })
+
   return (
     <Box sx={{ flex: 4, padding: 4, width: '100%', boxSizing: 'border-box'}}>
-      <Post/>
-      <Post/> 
-      <Post/> 
-      <Post/> 
+      <Post post={post} />
     </Box>
   )
 }
