89119712fbad252e23e6426b9871e8b7c85539f1
David Poslušný <david.poslusny@email.cz>
2022-08-25 19:18:09 +0200

    added possibility to create posts/recipes with axios post requests to API (only baseline so far)

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -3,15 +3,11 @@ import {
   Modal, 
   styled, 
   Typography,  
-  Divider, 
-  TextField, 
-  Button, 
-  ButtonGroup,
   SpeedDial,
   SpeedDialIcon,
   SpeedDialAction
 } from '@mui/material'
-import { Send, Restaurant, Description, Forum } from '@mui/icons-material'
+import { Restaurant, Description, Forum } from '@mui/icons-material'
 import React, { useState } from 'react'
 import { UserAvatar, UserText } from './Rightbar'
 import { Recipe } from './content/Recipe'
@@ -27,7 +23,8 @@ const CustomModal = styled(Modal)({
 const ModalBox = styled(Box)({
   display: 'flex',
   alignItems: 'center',
-  padding: 16,
+  marginBottom: 20,
+  marginTop: 20
 })
 
 export const Create = () => {
@@ -39,20 +36,20 @@ export const Create = () => {
   ];
 
   const [postType, setPostType] = useState('')
-  const [openDial, setOpenDial] = useState(false);
-  const [openModal, setOpenModal] = useState(false);
+  const [openDial, setOpenDial] = useState(false)
+  const [openModal, setOpenModal] = useState(false)
 
-  const handleOpenDial = () => setOpenDial(true);
-  const handleCloseDial = () => setOpenDial(false);
-  const handleOpenModal = () => setOpenModal(true);
-  const handleCloseModal = () => setOpenModal(false);
+  const handleOpenDial = () => setOpenDial(true)
+  const handleCloseDial = () => setOpenDial(false)
+  const handleOpenModal = () => setOpenModal(true)
+  const handleCloseModal = () => setOpenModal(false)
 
   return (
     <React.Fragment>
         <SpeedDial
           ariaLabel="Create a new post"
           icon={<SpeedDialIcon />}
-          onClick={handleOpenDial}
+          onOpen={handleOpenDial}
           onClose={handleCloseDial}
           open={openDial}
           sx={{ 
@@ -67,23 +64,23 @@ export const Create = () => {
           icon={action.icon}
           tooltipTitle={action.tooltip}
           onClick={e => {
-            e.stopPropagation();
             setPostType(action.name)
             handleOpenModal()
+            e.stopPropagation()
           }
           } 
         />
         ))}
         </SpeedDial>  
         <CustomModal
+        disableRestoreFocus
         open={openModal}
         onClose={handleCloseModal}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <Box width={600} height={450} bgcolor={'background.default'} color={'text.primary'} borderRadius={5} p={3}>
+        <Box width={600} height={'auto'} bgcolor={'background.default'} color={'text.primary'} borderRadius={5} p={3}>
           <Typography textAlign='center' variant='h6'>Create new {postType}</Typography>
-          <Divider sx={{marginBottom: 2, marginTop: 2}}/>
           <ModalBox>
             <UserAvatar
               sx={{marginRight: 1.5}}
@@ -99,27 +96,11 @@ export const Create = () => {
               David Poslušný
             </UserText>    
           </ModalBox>
-          <TextField
-          sx={{width: '100%'}}
-          id="standard-textarea"
-          multiline
-          rows={4}
-          placeholder="Default Value"
-          variant="standard"
-        />
         <Typography variant='p'>
-          {postType === 'recipe' && <Recipe/>}
+          {postType === 'recipe' && <Recipe handleCloseModal={handleCloseModal}/>}
           {postType === 'review' && <Review/>} 
           {postType === 'thread' && <Thread/>}             
-        </Typography>
-        <ButtonGroup
-          fullWidth
-          sx={{marginTop: 2}}
-        >
-          <Button variant="contained" endIcon={<Send />}>
-            Send
-          </Button>             
-        </ButtonGroup>    
+        </Typography>   
         </Box>
       </CustomModal> 
     </React.Fragment> 
--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,5 +1,5 @@
 import { Box } from '@mui/material'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Post } from './Post'
 import axios from 'axios'
 
@@ -7,9 +7,11 @@ export const Feed = () => {
 
   const [post, setPost] = useState([])
 
-  axios.get('https://daiton-food-square-api.herokuapp.com/api/v1/posts/getAll').then((response) => {
-    setPost(response.data)
-  })
+  useEffect(() => {
+    axios.get('https://daiton-food-square-api.herokuapp.com/api/v1/posts/getAll').then((response) => {
+      setPost(response.data)
+    })
+  }, [])
 
   return (
     <Box sx={{ flex: 4, padding: 4, width: '100%', boxSizing: 'border-box'}}>
